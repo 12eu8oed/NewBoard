1a6b03525e32436104d94c151576307b52f10b24
12eu8oed <150359214+12eu8oed@users.noreply.github.com>
2023-12-14 01:26:55 +0900

    redirect가 무엇인지 정확하게 알자

--- CRUDboard/src/main/java/com/board/controller/BoardController.java
+++ CRUDboard/src/main/java/com/board/controller/BoardController.java
@@ -1,12 +1,9 @@
 package com.board.controller;
 
 import java.util.List;
-import java.util.Locale;
 
 import javax.inject.Inject;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
