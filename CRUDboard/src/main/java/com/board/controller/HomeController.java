f49ed78593b917f526c1e6dbf8c158537cf06534
12eu8oed <150359214+12eu8oed@users.noreply.github.com>
2023-12-14 17:13:19 +0900

    쓸데없는 주석철 지우기전

--- CRUDboard/src/main/java/com/board/controller/HomeController.java
+++ CRUDboard/src/main/java/com/board/controller/HomeController.java
@@ -1,6 +1,5 @@
 package com.board.controller;
 
-import java.util.List;
 import java.util.Locale;
 
 import org.slf4j.Logger;
@@ -10,10 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import com.board.domain.BoardVO;
-import com.board.domain.Page;
 import com.board.service.BoardService;
 
 /**
@@ -24,8 +20,8 @@ public class HomeController {
 
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
-	@Autowired
-	private BoardService service; // BoardService를 주입받습니다.
+//	@Autowired
+//	private BoardService service; // BoardService를 주입받습니다.
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) throws Exception {
@@ -41,24 +37,24 @@ public class HomeController {
 	}
 
 //	 기존의 listPage 메소드는 그대로 둡니다.
-	@RequestMapping(value = "/listPage", method = RequestMethod.GET)
-	public void getListPage(Model model, @RequestParam("num") int num) throws Exception {
-		Page page = new Page();
-
-		page.setNum(num);
-		page.setCount(service.count());
-
-		List<BoardVO> list = null;
-		list = service.listPage(page.getDisplayPost(), page.getPostNum());
-
-		model.addAttribute("list", list);
-
-		// page 데이터 전부를 view로 전달한다.
-		model.addAttribute("page", page);
-
-		// 현재 페이지
-		model.addAttribute("select", num);
-
-	}
-
+//	@RequestMapping(value = "/listPage", method = RequestMethod.GET)
+//	public void getListPage(Model model, @RequestParam("num") int num) throws Exception {
+//		Page page = new Page();
+//
+//		page.setNum(num);
+//		page.setCount(service.count());
+//
+//		List<BoardVO> list = null;
+//		list = service.listPage(page.getDisplayPost(), page.getPostNum());
+//
+//		model.addAttribute("list", list);
+//
+//		// page 데이터 전부를 view로 전달한다.
+//		model.addAttribute("page", page);
+//
+//		// 현재 페이지
+//		model.addAttribute("select", num);
+//
+//	}
+//
 }
